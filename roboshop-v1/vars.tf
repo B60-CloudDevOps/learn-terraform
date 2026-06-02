1b599a51f2c9ae5a61173156503b2c9dbe62ad90
CodingManoj <mvp2612@hotmail.com>
2026-06-01 22:34:12 -0400

    Hashicorp terraform - basics

--- roboshop-v1/vars.tf
+++ roboshop-v1/vars.tf
@@ -1,3 +1,11 @@
 variable "components" {
-    default = ["catalogue", "cart", "user", "shipping", "payment", "frontend"]
+    default = ["catalogue", "shipping", "payment", "frontend"]
+}
+
+variable "ami_id" {
+    default = "ami-0fcc78c828f981df2"
+}
+
+variable "instance_type" {
+    default = "t3.medium"
 }
