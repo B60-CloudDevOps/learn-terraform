dfb0814c3bad9fd21c73742ccffc6c96da470a84
CodingManoj <mvp2612@hotmail.com>
2026-06-01 22:18:20 -0400

    Hashicorp terraform - basics

--- 06-functions/sample.tf
+++ 06-functions/sample.tf
@@ -8,3 +8,7 @@ output "fruits" {
 output "appleProperties" {
     value = "Apple is ${var.fruits["apple"]["color"]} in color, tastes ${var.fruits["apple"]["taste"]}, costs ${var.fruits["apple"]["price"]} per ${var.fruits["apple"]["metrics"]} and is available in ${var.fruits["apple"]["state"]}"
 }
+
+output "blueberryProperties" {
+    value = "Blueberries are ${var.fruits["blueberry"]["color"]} in color, taste ${var.fruits["blueberry"]["taste"]}, cost ${var.fruits["blueberry"]["price"]} per ${var.fruits["blueberry"]["metrics"]} and are available in ${var.fruits["blueberry"]["state"]}"
+}
