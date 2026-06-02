a8a0625a8de31107a126c90d7eb4111f8ec80fe6
CodingManoj <mvp2612@hotmail.com>
2026-06-01 21:41:51 -0400

    Hashicorp terraform - basics

--- 03-vars/vars.tf
+++ 03-vars/vars.tf
@@ -27,3 +27,18 @@ output "fruits" {
 output "favFruit" {
     value = "Favourite fruit is ${var.fruits[1]}"
 }
+
+# Example of map variable
+variable "person" {
+    type = map(string)
+    description = "Details of a person"
+    default = {
+        name = "Alice"
+        city = "New York"
+        age  = "30"
+    }
+}
+
+output "person" {
+    value = var.person
+}
