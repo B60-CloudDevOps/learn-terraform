049142716316123567268ee8f6d6d9ca8565ec38
CodingManoj <mvp2612@hotmail.com>
2026-06-03 22:01:01 -0400

    Hashicorp terraform - basics

--- 09-modules/main.tf
+++ 09-modules/main.tf
@@ -1,7 +1,7 @@
 module "demo-ec2" {
-    source = "./modules"   # Path of the module, it can be local or remote (github, gitlab, bitbucket, terraform registry)
+    source        = "./modules"   # Path of the module, it can be local or remote (github, gitlab, bitbucket, terraform registry)
     instance_type = var.instance_type
     instance_name = var.instance_name
-    ami_name = var.ami_name
-} 
+    ami_name      = var.ami_name
 
+}
--- 09-modules/modules/vars.tf
+++ 09-modules/modules/vars.tf
@@ -1,3 +1,4 @@
+# Empty Variables
 variable "instance_type" {}
 variable "instance_name" {}
 variable "ami_name" {}
