1fa3a11444058de1883c3c9e7303d5130998b869
CodingManoj <mvp2612@hotmail.com>
2026-06-03 21:32:36 -0400

    Hashicorp terraform - basics

--- 08-dataSource/data.tf
+++ 08-dataSource/data.tf
@@ -2,7 +2,13 @@ data "aws_security_group" "selected" {
   name = "b60-allow-all"
 }
 
-# Output the security group ID
-output "security_group_id" {
+# Output the security group info 
+output "security_group_info" {
   value = data.aws_security_group.selected
+}
+
+
+# outputs the securitu group id only
+output "security_group_id" {
+  value = data.aws_security_group.selected.id
 }
