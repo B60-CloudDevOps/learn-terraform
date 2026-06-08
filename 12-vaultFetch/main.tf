21358c48d5da74424da186dc14988628a8562244
CodingManoj <mvp2612@hotmail.com>
2026-06-07 22:53:57 -0400

    Hashicorp terraform - basics

--- 12-vaultFetch/main.tf
+++ 12-vaultFetch/main.tf
@@ -10,6 +10,6 @@ output "ssh_info" {
 
 # creating a file and saving the output info to that file.
 resource "local_file" "ssh_info_file" {
-  content  = data.vault_generic_secret.ssh_info.data
+  content  = data.vault_generic_secret.ssh_info
   filename = "/tmp/ssh_info.txt"
 }
