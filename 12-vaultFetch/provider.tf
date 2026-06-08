574eb208cd96396918ca3ae4c5cbef0ec988bc9d
CodingManoj <mvp2612@hotmail.com>
2026-06-07 22:46:33 -0400

    Hashicorp terraform - basics

--- 12-vaultFetch/provider.tf
+++ 12-vaultFetch/provider.tf
@@ -2,3 +2,11 @@ provider "vault" {
   url        = "http://vault-general.robotshop.fun:8200"
   auth_token = var.token
 }
+
+terraform {
+  backend "s3" {
+    bucket = "b60-s3-for-tfstate"
+    key    = "vault-demo/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
