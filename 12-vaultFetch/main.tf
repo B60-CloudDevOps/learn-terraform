8770ab5e6e47a3d83652d06e743997934fa82af4
CodingManoj <mvp2612@hotmail.com>
2026-06-07 22:53:31 -0400

    Hashicorp terraform - basics

--- 12-vaultFetch/main.tf
+++ 12-vaultFetch/main.tf
@@ -2,7 +2,14 @@ data "vault_generic_secret" "ssh_info" {
   path = "kv/sshcred"
 }
 
+# We cannot print the sensitive info on the screen directly and vault restricts that, which is a good aciton.
 output "ssh_info" {
   value = data.vault_generic_secret.ssh_info.data
   sensitive = true
+}
+
+# creating a file and saving the output info to that file.
+resource "local_file" "ssh_info_file" {
+  content  = data.vault_generic_secret.ssh_info.data
+  filename = "/tmp/ssh_info.txt"
 }
