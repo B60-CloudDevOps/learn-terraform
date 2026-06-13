102366b42c41d69c126e914bc07bc3ca39898d10
CodingManoj <mvp2612@hotmail.com>
2026-06-12 21:59:25 -0400

    Hashicorp terraform - Updating the playbook to extract the secrets

--- roboshop-v5-multiEnv/modules/app.tf
+++ roboshop-v5-multiEnv/modules/app.tf
@@ -13,7 +13,7 @@ resource "null_resource" "app" {
         inline = [
             "pip3.11 install ansible",
             "type ansible",
-            "ansible-pull -U https://github.com/B60-CloudDevOps/roboshop-ansible.git roboshop/roboshop-pull.yml -e env=${var.env_name} -e component=${var.name}"
+            "ansible-pull -U https://github.com/B60-CloudDevOps/roboshop-ansible.git roboshop/roboshop-pull.yml -e env=${var.env_name} -e component=${var.name} -e token=${var.vault_token}"
         ]
     }
 }
