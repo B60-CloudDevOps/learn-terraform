7df8d742fb483dde9d410f14eaf604763872ed1c
CodingManoj <mvp2612@hotmail.com>
2026-06-21 21:41:00 -0400

    EKS - provisions eks cluster and nodegroup

--- roboshop-v02-eks/init.sh
+++ roboshop-v02-eks/init.sh
@@ -4,5 +4,5 @@ action=$1
 environment=$2
 rm -rf .terraform ;
 terraform init --backend-config=env/${environment}/state.tfvars ;
-terraform plan -var-file=env/${environment}/${environment}.tfvars
-terraform $action -auto-approve -var-file=env/${environment}/${environment}.tfvars
+terraform plan --var-file=env/${environment}/${environment}.tfvars
+terraform $action -auto-approve --var-file=env/${environment}/${environment}.tfvars
