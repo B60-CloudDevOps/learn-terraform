283f8703be321a7ad21945a41f2a6baf12f2f82b
CodingManoj <mvp2612@hotmail.com>
2026-06-30 21:15:45 -0400

    K8S - Kubernetes Basics

--- roboshop-v02-eks/modules/eks/main.tf
+++ roboshop-v02-eks/modules/eks/main.tf
@@ -26,6 +26,9 @@ resource "aws_eks_addon" "cni" {
   cluster_name = aws_eks_cluster.main.name
   addon_name   = "vpc-cni"
   configuration_values = jsonencode({
-     enableNetworkPolicy = "true"
-  })
+     "enableNetworkPolicy" = "true"
+     "nodeAgent": {
+        "enablePolicyEventLogs" = "true"
+     }
+    })
 }
