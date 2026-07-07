24c1c91554dc1aaee4ae8abef3c42e6f4e8c4ed0
CodingManoj <mvp2612@hotmail.com>
2026-07-06 22:24:24 -0400

    Roboshop On EKS with DB's on Compute

---
+++ roboshop-on-eks/init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+action=$1
+environment=$2
+rm -rf .terraform ;
+terraform init --backend-config=env/${environment}/state.tfvars ;
+terraform plan --var-file=env/${environment}/${environment}.tfvars
+terraform $action -auto-approve --var-file=env/${environment}/${environment}.tfvars
